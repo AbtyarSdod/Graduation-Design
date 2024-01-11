08129a8ca04cfae17ae1c918034fb1bd0c77a4e7
Zoe <1526566980@qq.com>
2024-01-12 00:12:13 +0800

    [新增] 自动导入配置，scss配置 [更改] 删除Cli无用代码

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -1,10 +1,33 @@
+import { resolve } from 'node:path'
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig, splitVendorChunkPlugin } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
+
+const autoImportPath = resolve(__dirname, 'src')
 
 export default defineConfig({
   // splitVendorChunkPlugin构建分包策略
-  plugins: [vue(), splitVendorChunkPlugin()],
+  plugins: [
+    vue(),
+    splitVendorChunkPlugin(),
+    AutoImport({
+      // 自动导入 Vue Vue-Router 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue', 'vue-router'],
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [ElementPlusResolver()],
+      // d.ts 声明文件生成位置 src/auto-imports.d.ts 默认根目录 但无效果
+      dts: resolve(autoImportPath, 'auto-imports.d.ts')
+    }),
+    Components({
+      // 自动导入 Element Plus 组件
+      resolvers: [ElementPlusResolver()],
+      // d.ts 声明文件生成位置 src/components.d.ts 默认根目录 但无效果
+      dts: resolve(autoImportPath, 'components.d.ts')
+    })
+  ],
   server: {
     open: true, // 配置项目启动后，自动打开浏览器
     host: '0.0.0.0', // 配置Network 局域网访问
